7d11cc652018544437165c6d08394b25f78b0ea1
Azad Kemerbendi <kemerbendiazad@gmail.com>
2020-04-01 01:38:51 +0400

    FlyWeight

---
+++ structural/FlyWeight/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('Shape.php');
+require_once('Circle.php');
+require_once('ShapeFactory.php');
+$colours = array('red', 'blue', 'green', 'black', 'white', 'orange');
+$factory = new ShapeFactory();
+for ($i = 0; $i < 100; $i++) {
+    $randomColour = $colours[array_rand($colours)];
+    $circle = $factory->getCircle($randomColour);
+    $circle->setX(rand(0, 100));
+    $circle->setY(rand(0, 100));
+    $circle->setRadius(100);
+    $circle->draw();
+}
