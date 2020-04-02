9e2429e266b02559df38352b793758ad2c50aaf8
Azad Kemerbendi <kemerbendiazad@gmail.com>
2020-04-03 00:02:54 +0400

    observer

---
+++ behavioral/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once('Feed.php');
+require_once('Reader.php');
+$newspaper = new Feed('Junade.com');
+$allen = new Reader('Mark');
+$jim = new Reader('Lily');
+$linda = new Reader('Caitlin');
+//add reader
+$newspaper->attach($allen);
+$newspaper->attach($jim);
+$newspaper->attach($linda);
+//remove reader
+$newspaper->detach($linda);
+//set break outs
+$newspaper->breakOutNews('PHP Design Patterns');
