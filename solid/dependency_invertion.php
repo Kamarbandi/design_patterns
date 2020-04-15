9aac165080c4ee05a5f9492e399bcf027b3b9890
Azad Kemerbendi <kemerbendiazad@gmail.com>
2020-04-16 02:41:21 +0400

    dependency invertion last

--- solid/dependency_invertion.php
+++ solid/dependency_invertion.php
@@ -4,36 +4,35 @@ class Customer
 {
     private $currentOrder = null;
 
-    public function buyItems()
+    public function buyItems(IOrderProcessor $processor)
     {
-        if (is_null($this->currentOrder)) {
+        if(is_null($this->currentOrder)){
             return false;
         }
 
-        $processor = new OrderProcessor();
         return $processor->checkout($this->currentOrder);
     }
 
-    public function addItem($item)
-    {
-        if (is_null($this->currentOrder)) {
+    public function addItem($item){
+        if(is_null($this->currentOrder)){
             $this->currentOrder = new Order();
         }
         return $this->currentOrder->addItem($item);
     }
-
-    public function deleteItem($item)
-    {
-        if (is_null($this->currentOrder)) {
+    public function deleteItem($item){
+        if(is_null($this->currentOrder)){
             return false;
         }
-        return $this->currentOrder->deleteItem($item);
+        return $this->currentOrder ->deleteItem($item);
     }
 }
 
-class OrderProcessor
+interface IOrderProcessor
 {
-    public function checkout($order)
-    {/*...*/
-    }
+    public function checkout($order);
+}
+
+class OrderProcessor implements IOrderProcessor
+{
+    public function checkout($order){/*...*/}
 }
